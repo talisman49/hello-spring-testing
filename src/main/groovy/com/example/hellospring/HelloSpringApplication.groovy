7c294c25bc9444ac24c364d7b2a99e43fbcca645
Javier Arturo Rodriguez <jrodriguez@sinensia.com>
2021-07-28 08:57:53 +0200

    Initial commit

---
+++ src/main/groovy/com/example/hellospring/HelloSpringApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.hellospring
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class HelloSpringApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(HelloSpringApplication, args)
+    }
+
+}
